1812ef19820d621da47a70729d2d448f3d6a57b3
Ivan <cetona42@gmail.com>
2025-01-16 17:53:25 +0300

    no 1

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
-
+#include < stdint.h >
+#include < memory.h >
+#include < stdio.h >
+#include < stdbool.h >
+#include <stdlib.h>
+#include <malloc.h>
+#include <math.h>
+#include "arrayLib.h"
+#include "matrix.h"
 
 int main() {
+	matrix m = getMemMatrix(3, 3);
+	inputMatrix(&m);
 
+	position min = getMinValuePos(m);
+	position max = getMaxValuePos(m);
+
+	swapRows(m, min.rowIndex, max.rowIndex);
+
+	outputMatrix(m);
+
+	freeMemMatrix(&m);
 }
